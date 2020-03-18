1e5d594da7da0462396171496a771cd11709632a
aricson <aricsondr@gmail.com>
2020-03-18 15:34:32 -0100

    Initialize Spring EventListenerAPP

---
+++ src/main/java/com/example/eventlistenerapp/EventListenerappApplication.java
@@ -0,0 +1,13 @@
+package com.example.eventlistenerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventListenerappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventListenerappApplication.class, args);
+	}
+
+}
